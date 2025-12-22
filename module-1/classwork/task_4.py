59e776f5495fa5baa96a7e3520fa68a63080e746
Tahirrrm <145999547+Tahirrrm@users.noreply.github.com>
2025-12-22 20:33:58 +0300

    feat:обновление структуры

---
+++ module-1/classwork/task_4.py
@@ -0,0 +1,4 @@
+num_str = int(input  ("Введите размер основания треугольника : "))
+num2_str = int(input  ("Введите размер высоты треугольника : "))
+result = 0.5 * num_str * num2_str
+print (f"площаль треугольника {result } ")
