997de260779aaba81fff247b4f30457e22af3197
Tahirrrm <145999547+Tahirrrm@users.noreply.github.com>
2025-12-29 20:45:54 +0300

    feat: 29.12.2025

---
+++ module-2/classwork/task_2.py
@@ -0,0 +1,10 @@
+# pairs = int (input ( "Сколько пар ключ- значение необходимо создать?"))
+# print (pairs)
+# dictionary= {}
+
+# for i in range (pairs):
+
+#     key= input ( f"Введите ключ {i+1}")
+#     value = input (f"Введите значение для ключа `{key}`")
+#     dictionary[key]=value
+#     print (dictionary)
