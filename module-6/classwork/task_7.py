5b4a1ecb7a920574717ed87e2c459ea852e81094
Tahirrrm <145999547+Tahirrrm@users.noreply.github.com>
2026-02-04 20:49:32 +0300

    решение задач 04.02.

--- module-6/classwork/task_7.py
+++ module-6/classwork/task_7.py
@@ -1,3 +1,42 @@
 logs = [
-    ("ivan","day")
+    ("ivan","day",1),
+    ("ivan","night",23),
+    ("olga","day",1),
+    ("petr","night",7),
+    ("anna","day",1),
+    ("anna","day",1),
+
 ]
+shift_hours= {}
+employee_shift = {}
+shift_long = {}
+
+for log in logs:
+    if log[0] not in shift_long:
+        shift_long[log[1]] = 0
+        shift_long[log[1]] += log[2]
+
+    if log [1] not in shift_hours:
+        shift_hours[log[1]] = 0
+    shift_hours[log[1]] += log[2]
+
+    if log [0] not in employee_shift:
+        employee_shift [log[0]] = set()
+    employee_shift[log[0]].add(log[1])
+for employee in employee_shift:
+    value= employee_shift[employee]
+    if len(value)== 2:
+        print(employee)
+print(employee_shift)
+
+# пункт 2
+for shift in shift_hours:
+    if shift_hours[shift] < 8 :
+        print(f"на смене {shift} отработали меньше 8 часов")
+print(shift_hours)
+# пункт 3
+long = []
+for longs in shift_long:
+    if shift_long[longs] >=12:
+        long.append(longs)
+print (f"на смене {long} отработали больше 12 часов")
