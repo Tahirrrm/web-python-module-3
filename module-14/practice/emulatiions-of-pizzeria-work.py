7b31414064c90b2471be283c83b88d3c04d57b00
Tahirrrm <145999547+Tahirrrm@users.noreply.github.com>
2026-04-29 20:45:56 +0300

    cw: решение задач по модулю 14 от 29.04.2026

--- module-14/practice/emulatiions-of-pizzeria-work.py
+++ module-14/practice/emulatiions-of-pizzeria-work.py
@@ -5,7 +5,6 @@ from abc import ABC,abstractmethod
 class Ingredient:
     name: str
     key: str
-    category: str
     price: float
     cost: float
 
@@ -17,18 +16,20 @@ class Recipe:
 class RecipeFactory:
     def get_standard_recipes() -> dict[int, Recipe]:
         return {
-            0: Recipe("Пицца-1", ["dough","cheese","tomate","mayonaise","chiken"]),
-            1: Recipe("Пицца-2", ["dough","cheese","tomate","ketchup","chiken"]),
-            2: Recipe("Пицца-3", ["dough","cheese","tomate","chiken"]),
+            1: Recipe("Пицца-1", ["dough","cheese","tomate","mayonaise","chiken"]),
+            2: Recipe("Пицца-2", ["dough","cheese","tomate","ketchup","chiken"]),
+            3: Recipe("Пицца-3", ["dough","cheese","tomate","chiken"]),
         }
+    
 class PizzaBuilder:
     def __init__(self):
-        self._ingredient - ["dough","cheese"]
+        self._ingredient = ["dough","cheese"]
 
     def add_ingredient(self,key:str):
         if key not in self._ingredient:
             self._ingredient.append(key)
         return self
+    
     def build(self):
         return Recipe("Своя пицца",self._ingredient)
     
@@ -36,11 +37,13 @@ class PizzaBuilder:
 class OrderItem:
     recipe: Recipe
     quantity: int
+
     def total_price(self, ingredients: dict [int, Ingredient]) -> float:
         one_pizza_price = sum(
             ingredients[key].price for key in self.recipe.ingredient_keys
         )
         return one_pizza_price *self.quantity
+    
     def total_cost(self, ingredients: dict [int, Ingredient]) -> float:
         one_pizza_cost = sum(
             ingredients[key].price for key in self.recipe.ingredient_keys
@@ -64,45 +67,226 @@ class Order:
     def to_text(self, ingredients: dict[str, Ingredient]) -> str:
         lines = ["Информация о заказе:"]
         for item in self.items:
-            ingredients_names = [
-                Ingredient[key].name for key in item.recipe.ingredient_keys
+            ingredients_name = [
+                ingredients[key].name for key in item.recipe.ingredient_keys
             ]
             lines.append(f"Пицца: {item.recipe.name}")
             lines.append(f"Количество: {item.quantity}")
             lines.append("Состав:")
-            for name in ingredients_names:
+            for name in ingredients_name:
                 lines.append(f"-{name}")
             lines.append(f"Цена позиции: {item.total_price(ingredients)} руб.")
             lines.append(f"Способ  оплаты: {self.payment_type}")
 
         return "\n".join(lines)
     
-    class PaymentStrategy(ABC):
-        @abstractmethod
-        def pay(self,amount:float):
-            pass
-    class CashPayment(PaymentStrategy):
-        def pay(self,amount:float):
-            return f"Оплата наличными выполнена на сумму {amount} руб"
+class PaymentStrategy(ABC):
+    @abstractmethod
+    def pay(self,amount:float):
+        pass
+class CashPayment(PaymentStrategy):
+    def pay(self,amount:float):
+        return f"Оплата наличными выполнена на сумму {amount} руб"
         
-    class CardPayment(PaymentStrategy):
-        def pay(self,amount:float):
-            return f"Оплата картой выполнена на сумму {amount} руб"
+class CardPayment(PaymentStrategy):
+    def pay(self,amount:float):
+        return f"Оплата картой выполнена на сумму {amount} руб"
         
-    class FileOrderSaver:
-        def __init__(self,filename: str = "order.txt"):
-            self.filename = filename
-        def save(self, order: Order, ingredients: dict[str, Ingredient]):
-            with open(self.filename, "a", encoding="utf-8") as file:
-                file.write(order.to_text(ingredients))
-                file.write("\n" + "-" *50 + "\n")
+class FileOrderSaver:
+    def __init__(self,filename: str = "order.txt"):
+        self.filename = filename
+    def save(self, order: Order, ingredients: dict[str, Ingredient]):
+        with open(self.filename, "a", encoding="utf-8") as file:
+            file.write(order.to_text(ingredients))
+            file.write("\n" + "-" *50 + "\n")
 
-    def create_ingredients():
-        return{
-            "dough": Ingredient("Тесто", "dough", 70,30),
-            "cheese": Ingredient("Сыр", "cheese", 80,20),
-            "tomate": Ingredient("Помидор", "tomate",20,5),
-            "mayonaise": Ingredient("Майонез", "mayonaise",50,10),
-            "chiken": Ingredient("курица", "chiken",100,50),
-            "ketchup": Ingredient("кетчуп", "ketchup",15,3),
+def create_ingredients():
+    return {
+        "dough": Ingredient("Тесто", "dough", 70,30),
+        "cheese": Ingredient("Сыр", "cheese", 80,20),
+        "tomate": Ingredient("Помидор", "tomate",20,5),
+        "mayonaise": Ingredient("Майонез", "mayonaise",50,10),
+        "chiken": Ingredient("курица", "chiken",100,50),
+        "ketchup": Ingredient("кетчуп", "ketchup",15,3),
         }
+
+def create_stock() -> dict[str,int]:
+    return{
+                "dough": 10,
+                "cheese": 10,
+                "tomate": 10,
+                "mayonaise": 10,
+                "chiken": 10,
+                "ketchup": 10
+            }
+        
+
+def get_topping() -> list[str]:
+    return  [
+                "tomate",
+                "mayonaise",
+                "chiken",
+                "ketchup"
+
+        ]
+def create_custom_recipe(inventory: Inventory) -> Recipe:
+    builder = PizzaBuilder()
+    print("Создание своей пиццы")
+
+    for key in get_topping():
+        ingredient = inventory.ingredients[key]
+        choice = input( f"Хотите добавить {ingredient.name}")
+        if choice == "да":
+            builder.add_ingredient(ingredient.key)
+    return builder.build()
+    
+class Inventory:
+    def __init__(self, ingredients: dict[str,Ingredient], stock: dict[str,int]):
+        self.ingredients = ingredients
+        self.stock = stock
+
+        
+    def has_enough(self,ingredient_keys: list[str],quantity: int) -> bool:
+        for key in ingredient_keys:
+            if self.stock.get(key,0) < quantity:
+                return False
+        return True
+        
+
+    def reduce_stock(self, ingredient_keys: list[str], quantity: int) :
+        for key in ingredient_keys:
+            self.stock[key] -= quantity
+
+    def show(self):
+        print("Наличие ингредиентов")
+
+        for key,count in self.stock.items():
+            ingredient = self.ingredients[key]
+            print(f"{ingredient.name}: {count}")
+    
+
+class SalesReport:
+    def __init__(self):
+        self.profit = 0
+        self.revenue = 0
+        self.sold_count = 0
+            
+    def add_order(self,order: Order,ingredients: dict [str,Ingredient]):
+        self.sold_count += sum(item.quantity for item in order.items)
+        self.revenue += order.total_price(ingredients)
+        self.profit += order.total_profit(ingredients)
+
+    def show(self):
+        print("Отчет")
+        print(f"Продано пицц:{self.sold_count}")
+        print(f"Выручка:{self.profit}")
+        print(f"Доход:{self.revenue}")
+
+def show_menu():
+
+        print("1. Создать заказ")
+        print("2. Отчет")   
+        print("3. Наличие ингредиентов") 
+        print("4. Выход") 
+
+def show_standard_recipes(
+        recipes: dict[int, Recipe], 
+        ingredients: dict[str, Ingredient]):
+        
+    print ("Стандартные пиццы")
+
+    for number,recipe in recipes.items():
+        print (f"{number}, {recipe.name}")
+    price = sum(ingredients[key].price for key in recipe.ingredient_keys)
+    print (f"Цена за штуку: {price}")
+
+def choose_recipe(
+    recipe :dict [int,Recipe],
+    ingredients: dict [str,Ingredient] ,
+    inventory: Inventory
+    ):
+        
+    while True:
+        choice = input ("Выберите вариант:")
+        if choice == "0":
+
+            return create_custom_recipe(inventory)
+        else:
+            return recipe[int(choice)]
+    
+def choose_payment():
+        
+    print("Выберите способ оплаты:")
+    print("1 - наличные")
+    print("2 - Карта")
+
+    while True:
+
+        choice = input(" Выберите:")
+
+        if choice == "1":
+            return CashPayment(), "Наличные"
+        elif choice == "2":
+            return CardPayment(), "Карта"
+        print("выберите команду 1 и 2 вариантом")
+
+def create_order(
+        ingredients: dict [str,Ingredient],
+        inventory: Inventory,
+        report: SalesReport,
+        file_saver: FileOrderSaver
+    ):
+    recipes = RecipeFactory.get_standard_recipes()
+    items: list[OrderItem] = []
+
+    while True:
+
+        show_standard_recipes(recipes,ingredients)
+
+        recipe = choose_recipe(recipes,ingredients,inventory)
+        quantity = int(input("Введите количество:"))
+            
+
+        if not inventory.has_enough(recipe.ingredient_keys,quantity):
+            print("Недостаточно ингредиентов для заказа.")
+            return
+        items.append(OrderItem(recipe,quantity))
+
+        more = input("Добавить еще один вид пиццы?")
+
+        if more == "нет":
+            break
+    payment_strategy, payment_type = choose_payment()
+    order = Order(items,payment_type)
+        
+    for item in items:
+        inventory.reduce_stock(item.recipe.ingredient_keys, item.quantity)
+    amount = order.total_price(ingredients)
+    print(payment_strategy.pay(amount))
+    file_saver.save(order,ingredients)
+    report.add_order(order,ingredients)
+
+def main():
+
+    ingredients = create_ingredients()
+    stock = create_stock()
+
+    inventory = Inventory(ingredients,stock)
+    report = SalesReport()
+    file_saver = FileOrderSaver()
+
+    while True:
+        show_menu()
+
+        choice = input ("Выберите пункт меню")
+
+        if choice == "1":
+            create_order(ingredients,inventory,report,file_saver)
+        if choice =="2":
+            report.show()
+        if choice == "3":
+            inventory.show()
+        elif choice == "4":
+            break
+
+main()
