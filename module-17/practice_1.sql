af576d47e93f2fe9452b0b7a460305dfe11167c2
Tahirrrm <145999547+Tahirrrm@users.noreply.github.com>
2026-05-25 20:42:39 +0300

    cw
    cw

--- module-17/practice_1.sql
+++ module-17/practice_1.sql
@@ -103,5 +103,110 @@ DELETE FROM projects
 WHERE is_active = false
 returning
 id,name;
+create table employee_profiles (
+id SERIAL primary key, 
+employee_id INT unique references employees(id), 
+phone TEXT unique,
+address TEXT,
+birth_date DATE);
 
+insert into employee_profiles( employee_id,phone,address, birth_date)
+values 
+	(1, '+70000000000', 'address-1', '1980-05-25'),
+	(2, '+70000000001', 'address-1', '1981-05-25'),
+	(3, '+70000000002', 'address-1', '1982-05-25');
+
+select 
+e.name as employee_name,
+ep.phone,
+ep.address,
+ep.birth_date
+from employees e
+join employee_profiles ep on ep.employee_id = e.id;
+
+insert into employee_profiles(employee_id,phone,address,birth_date)
+values 
+(1, '+70000000003', 'address-4', '1995-05-25');
+
+--------
+----N---N
+
+create table skills( 
+id SERIAL primary key,
+name TEXT not null unique);
+
+create  table employee_skills(
+employee_id INT references employees(id),
+skill_id int references skills(id),
+primary key(employee_id,skill_id)
+);
+
+insert into skills(name)
+values 
+('SQL'),
+('PostgreSQL'),
+('MySQL'),
+('Excel');
+
+insert into employee_skills(employee_id,skill_id)
+values 
+	(1,1),
+	(2,1),
+	(3,1),
+	(1,2),
+	(2,2),
+	(3,2),
+	(1,4);
+
+select 
+	e.name as employee_name,
+	s.name as skill_name
+from employee_skills es
+join employees e on es.employee_id = e.id 
+join skills s on es.skill_id = s.id
+order by e.name,s.name;
+-------------
+
+select 
+e.name as employee_name,
+e.salary,
+d.name as department_name,
+ep.phone,
+ep.address,
+p.name as project_name,
+s.name as skill_name
+from employees e
+left join departments d ON e.id = d.id
+left join employee_profiles ep on e.id = ep.id
+left join projects p on e.id = p.id
+left join employee_skills es on es.employee_id = e.id
+left join skills s on es.skill_id = s.id
+order by e.name, p.name, s.name;
+
+select 
+e.name as employee_name,
+coalesce(SUM(p.budget),0) as total_budget
+from employees e
+left join projects p on e.id = p.id
+group by e.name
+order by e.name;
+
+select 
+p.name as project_name,
+p.budget ,
+e.name as employee_name,
+d.name as department_name
+from projects p
+join employees e on e.id = p.employee_id 
+join departments d on d.id = e.departament_id 
+where is_active = true and p.budget > 200000
+order by p.budget  desc;
+
+select
+e.name as emp_name,
+e.salary,
+d.name as dep_name,
+p.name as proj_name,
+p.budget 
+from employees e
 
